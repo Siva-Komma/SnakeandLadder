2c89e752367ccc12df48a83138effbafcfdac302
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-09 20:58:37 +0530

    Displaying welcome message of Snake and Ladder

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder");
+            Console.ReadLine();
+        }
+    }
+}
