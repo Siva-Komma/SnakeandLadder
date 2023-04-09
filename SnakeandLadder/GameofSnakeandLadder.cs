688ea041ceaa6a81470989f17ea0bf2faec70e3f
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-09 21:05:26 +0530

    Single Player Position

---
+++ SnakeandLadder/GameofSnakeandLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class GameofSnakeandLadder
+    {
+        public static void Game()
+        {
+            int Position = 0;
+            Console.WriteLine($"Player Position: {Position}");
+        }
+    }
+}
